2e1b586b5c7b71fd1122d80eb7a98e95040ed91d
littlemac08 <128768517+littlemac08@users.noreply.github.com>
2023-03-28 20:09:51 +0900

    Add files via upload

--- basic.js
+++ basic.js
@@ -1,12 +1,32 @@
-const elGnbList = document.querySelectorAll('.nav .gnb_main')
-const elDeps1 = document.querySelector('.nav .gnb_main .dep1_wrap')
+const slides = document.querySelector('.banner_wrap')
+const slideContent = document.querySelectorAll('.banner_wrap .slider .slider_content')
+const ContentWidth = slideContent[0].offsetWidth;
+const ContentCount = slideContent.length;
+console.log(ContentCount)
+const prevBtn = document.querySelector('.slideBtn_left')
+const nextBtn = document.querySelector('.slideBtn_right')
 
-let num = 0;
+let currentIdx = 0;
 
-elGnbList.forEach(function(li,index){
-    elGnbList[index].addEventListener('mouseover',function(){
-        elGnbList[index]
-        console.log(elGnbList[index])
-    })
+nextBtn.addEventListener('click', next)
+prevBtn.addEventListener('click', prev)
 
-})
+function next(){
+    if(currentIdx < ContentCount-1){
+        slides.style.left = `-${(currentIdx+1)* ContentWidth}px`
+        currentIdx += 1;
+    } else if(currentIdx = ContentCount-1){
+        slides.style.left = `0px`
+        currentIdx = 0;
+    }
+}
+function prev(){
+    if(currentIdx === 0){
+        slides.style.left = `-${(ContentCount-1)* ContentWidth}px`;
+        currentIdx = ContentCount-1;
+    } else if(currentIdx > 0){
+        slides.style.left = `-${(currentIdx-1)* ContentWidth}px`;
+        currentIdx = currentIdx-1;
+    }
+}
+
